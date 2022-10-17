2c2f0e1f1765e2c6c33743b036134d151f2e5d01
SRoss <srossouw@uwm.edu>
2022-10-17 18:02:13 -0500

    added react state components

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { StoreProvider } from "./utils/GlobalState";
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
@@ -40,15 +41,35 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
+          <StoreProvider>
+            <Nav />
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />}
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/orderHistory" 
+                element={<OrderHistory />} 
+              />
+              <Route 
+                path="/products/:id" 
+                element={<Detail />}
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />}
+              />
+            </Routes>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
